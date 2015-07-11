b76ee161b7c099f2db85d1fcd1feb19e263fcbed
Cina Saffary <csaffary@umbel.com>
2015-07-10 23:37:35 -0500

    fixing /repos endpoint to return pr_list

--- velocity_prep/deploy_api/controllers/DeploysService.js
+++ velocity_prep/deploy_api/controllers/DeploysService.js
@@ -8,68 +8,33 @@ var PRs = require('./PRsService');
 
 var DEPLOYS_FILEPATH = path.join(__dirname, '../csv/deploys.csv')
 
-function prCallback(pr_list){
-  console.log("current list", pr_list)
-  return function(data){
-    console.log("current data", data)
-    pr_list.push(data)
-  }
-}
-function logit(string, data){
-  console.log(string, data)
-}
-
-
-function findPR(pr_id) {
-    var ret;
-    setTimeout
-    PRs.findPRbyID(pr_id, function(data){
-      logit("findPRbyID", data)
-      ret = data
+function getPRSforDeploys(deploys) {
+  var promises = deploys.deploy_list.map(function (deploy) {
+    var prs = deploy.prs.split(' ')
+    return new Promise(function (resolve, reject) {
+      PRs.findPRsbyIDs(prs, resolve)
+    }).then(function (prs) {
+      deploy.prs = prs
+      return deploy
     })
-    setTimeout( function (){
-      logit("i am returning this", ret)
-      return ret
-    }, 2000)
-    
-}
+  })
 
+  return Promise.all(promises).then(function (results) {
+    return deploys
+  })
+}
 
 function readDeploysFile(callback) {
   var fileStream = fs.createReadStream(DEPLOYS_FILEPATH);
   var converter = new Converter({constructResult:true});
 
-  var deploy_list = {}
- 
-  converter.on("record_parsed", function(data){
-    var prs = data.prs.split(" ")
-    console.log("prs", prs)
-    //console.log("test call", findPR('123'))
-    pr_list = []
-    for (var i = 0; i < prs.length; i++){
-      pr = findPR(prs[i]);
-      console.log("pushing pr to list", pr)
-      pr_list.push(pr)
-    }
-    console.log(pr_list)
-
-    
-    //async.map(findPR(pr_list), function (err, stuff){ return stuff});
-    //console.log("pr_list",pr_list)
-
-  })
-
   converter.on("end_parsed", function (data) {
-    
-
-
     var deploys = {}
 
-
     deploys.deploy_list = data
-    deploys.count   = data.length
+    deploys.count = data.length
 
-    callback(deploys)
+    getPRSforDeploys(deploys).then(callback)
   })
 
   fileStream.pipe(converter);
